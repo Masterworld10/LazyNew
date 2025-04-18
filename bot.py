83cf58e0e63d920fa93142d1aaf7e0f4e453aa98
Masterworld10 <157733278+Masterworld10@users.noreply.github.com>
2025-04-18 20:03:24 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,66 +1,81 @@
-import logging import logging.config import threading
+import logging
+import logging.config
+import threading
 
-Get logging configurations
+# Get logging configurations
+logging.config.fileConfig('logging.conf')
+logging.getLogger().setLevel(logging.INFO)
+logging.getLogger("pyrogram").setLevel(logging.ERROR)
+logging.getLogger("imdbpy").setLevel(logging.ERROR)
 
-logging.config.fileConfig('logging.conf') logging.getLogger().setLevel(logging.INFO) logging.getLogger("pyrogram").setLevel(logging.ERROR) logging.getLogger("imdbpy").setLevel(logging.ERROR)
-
-from pyrogram import Client, version from pyrogram.raw.all import layer from database.ia_filterdb import Media from database.users_chats_db import db from info import SESSION, API_ID, API_HASH, BOT_TOKEN, LOG_STR from utils import temp from typing import Union, Optional, AsyncGenerator from pyrogram import types from aiohttp import web from plugins import web_server from plugins import ott_notifier  # Import the OTT notifier
+from pyrogram import Client, __version__
+from pyrogram.raw.all import layer
+from database.ia_filterdb import Media
+from database.users_chats_db import db
+from info import SESSION, API_ID, API_HASH, BOT_TOKEN, LOG_STR
+from utils import temp
+from typing import Union, Optional, AsyncGenerator
+from pyrogram import types
+from aiohttp import web
+from plugins import web_server
+from plugins import ott_notifier  # NEW LINE
 
 PORT = "8080"
 
 class Bot(Client):
 
-def __init__(self):
-    super().__init__(
-        name=SESSION,
-        api_id=API_ID,
-        api_hash=API_HASH,
-        bot_token=BOT_TOKEN,
-        workers=50,
-        plugins={"root": "plugins"},
-        sleep_threshold=5,
-    )
+    def __init__(self):
+        super().__init__(
+            name=SESSION,
+            api_id=API_ID,
+            api_hash=API_HASH,
+            bot_token=BOT_TOKEN,
+            workers=50,
+            plugins={"root": "plugins"},
+            sleep_threshold=5,
+        )
 
-async def start(self):
-    b_users, b_chats = await db.get_banned()
-    temp.BANNED_USERS = b_users
-    temp.BANNED_CHATS = b_chats
-    await super().start()
-    await Media.ensure_indexes()
-    me = await self.get_me()
-    temp.ME = me.id
-    temp.U_NAME = me.username
-    temp.B_NAME = me.first_name
-    self.username = '@' + me.username
-    app = web.AppRunner(await web_server())
-    await app.setup()
-    bind_address = "0.0.0.0"
-    await web.TCPSite(app, bind_address, PORT).start()
-    logging.info(f"{me.first_name} with for Pyrogram v{__version__} (Layer {layer}) started on {me.username}.")
-    logging.info(LOG_STR)
+    async def start(self):
+        b_users, b_chats = await db.get_banned()
+        temp.BANNED_USERS = b_users
+        temp.BANNED_CHATS = b_chats
+        await super().start()
+        await Media.ensure_indexes()
+        me = await self.get_me()
+        temp.ME = me.id
+        temp.U_NAME = me.username
+        temp.B_NAME = me.first_name
+        self.username = '@' + me.username
+        app = web.AppRunner(await web_server())
+        await app.setup()
+        bind_address = "0.0.0.0"
+        await web.TCPSite(app, bind_address, PORT).start()
+        logging.info(f"{me.first_name} with for Pyrogram v{__version__} (Layer {layer}) started on {me.username}.")
+        logging.info(LOG_STR)
 
-    # Start the OTT notifier in a background thread
-    threading.Thread(target=ott_notifier.send_new_releases, daemon=True).start()
+        # Start the OTT notifier in a background thread
+        threading.Thread(target=ott_notifier.send_new_releases, daemon=True).start()
 
-async def stop(self, *args):
-    await super().stop()
-    logging.info("Bot stopped. Bye.")
+    async def stop(self, *args):
+        await super().stop()
+        logging.info("Bot stopped. Bye.")
 
-async def iter_messages(
-    self,
-    chat_id: Union[int, str],
-    limit: int,
-    offset: int = 0,
-) -> Optional[AsyncGenerator["types.Message", None]]:
-    current = offset
-    while True:
-        new_diff = min(200, limit - current)
-        if new_diff <= 0:
-            return
-        messages = await self.get_messages(chat_id, list(range(current, current+new_diff+1)))
-        for message in messages:
-            yield message
-            current += 1
+    async def iter_messages(
+        self,
+        chat_id: Union[int, str],
+        limit: int,
+        offset: int = 0,
+    ) -> Optional[AsyncGenerator["types.Message", None]]:
+        current = offset
+        while True:
+            new_diff = min(200, limit - current)
+            if new_diff <= 0:
+                return
+            messages = await self.get_messages(chat_id, list(range(current, current+new_diff+1)))
+            for message in messages:
+                yield message
+                current += 1
 
-app = Bot() app.run()
 
+app = Bot()
+app.run()
